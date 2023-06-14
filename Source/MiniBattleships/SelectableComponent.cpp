28812c5776d7ff141d977a93e960f42c1adb2b4e
Paweł Mierzwa <cxnshx@gmail.com>
2023-06-14 17:17:05 +0200

    Add selection decals

---
+++ Source/MiniBattleships/SelectableComponent.cpp
@@ -0,0 +1,104 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SelectableComponent.h"
+#include "Components/DecalComponent.h"
+#include "Components/BoxComponent.h"
+
+// Sets default values for this component's properties
+USelectableComponent::USelectableComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+
+}
+
+
+// Called when the game starts
+void USelectableComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	float DecalHeight = GetOwnerCollisionHeight();
+	float DecalRadius = GetOwnerCollisionSize();
+
+	DecalComponent = GetOwner()->FindComponentByClass<UDecalComponent>();
+
+	//Decal rotation and size
+	DecalComponent->SetRelativeRotation(FRotator::MakeFromEuler(FVector(0.0f, -90.0f, 0.0f)));
+	DecalComponent->DecalSize = FVector(DecalHeight, DecalRadius, DecalRadius);
+
+	//Decal material
+	SelectionCircleMaterialInstance = UMaterialInstanceDynamic::Create(SelectionCircleMaterial, this);
+	DecalComponent->SetDecalMaterial(SelectionCircleMaterialInstance);
+
+	DecalComponent->SetHiddenInGame(true);
+}
+
+
+// Called every frame
+void USelectableComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+
+void USelectableComponent::SelectActor()
+{
+	if (bSelected) return;
+
+	bSelected = true;
+
+	// Update selection circle.
+	if (IsValid(DecalComponent))
+	{
+		DecalComponent->SetHiddenInGame(false);
+	}
+}
+
+void USelectableComponent::DeselectActor()
+{
+	if (!bSelected) return;
+
+	bSelected = false;
+
+	// Update selection circles.
+	if (IsValid(DecalComponent))
+	{
+		DecalComponent->SetHiddenInGame(true);
+	}
+}
+
+void USelectableComponent::HoverActor()
+{
+
+}
+
+void USelectableComponent::UnhoverActor()
+{
+
+}
+
+float USelectableComponent::GetOwnerCollisionSize()
+{
+	UBoxComponent* BoxComponent = GetOwner()->FindComponentByClass<UBoxComponent>();
+	if (!IsValid(BoxComponent))
+	{
+		UE_LOG(LogTemp, Error, TEXT("Cannot find collision box for %s"), *GetOwner()->GetName());
+		return 0.0f;
+	}
+
+	FCollisionShape CollisionShape = BoxComponent->GetCollisionShape();
+	return FMath::Max(CollisionShape.Box.HalfExtentX, CollisionShape.Box.HalfExtentY) * 2;
+}
+
+float USelectableComponent::GetOwnerCollisionHeight()
+{
+	UBoxComponent* BoxComponent = GetOwner()->FindComponentByClass<UBoxComponent>();
+	if (!IsValid(BoxComponent)) 
+	{
+		UE_LOG(LogTemp, Error, TEXT("Cannot find collision box for %s"), *GetOwner()->GetName());
+		return 0.0f;
+	}
+
+	FCollisionShape CollisionShape = BoxComponent->GetCollisionShape();
+	return CollisionShape.Box.HalfExtentZ * 2;
+}
---
+++ Source/MiniBattleships/SelectableComponent.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "SelectableComponent.generated.h"
+
+class UMaterialInstance;
+class UMaterialInstanceDynamic;
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class MINIBATTLESHIPS_API USelectableComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	USelectableComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UFUNCTION(BlueprintCallable)	
+	void SelectActor();
+	UFUNCTION(BlueprintCallable)
+	void DeselectActor();
+	UFUNCTION(BlueprintCallable)
+	void HoverActor();
+	UFUNCTION(BlueprintCallable)
+	void UnhoverActor();
+
+
+private:
+	float GetOwnerCollisionSize();
+	float GetOwnerCollisionHeight();
+
+	bool bSelected = false;
+	bool bHovered = false;
+
+	UMaterialInstanceDynamic* SelectionCircleMaterialInstance;
+	UMaterialInstance* SelectionCircleMaterial;
+
+	UPROPERTY()
+	UDecalComponent* DecalComponent;
+};
